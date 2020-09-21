108480392fbe8b3f5fbd0f61e03a3e8e172ef8be
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 16:27:19 +0100

    This commit add hero component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Layout from './components/Layout';
+import Hero from './components/Hero';
 import GlobalStyles from './GlobalStyles';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
     <GlobalStyles />
     <Layout>
-      
+      <Hero />
     </Layout>
     </>
   );
---
+++ src/components/Hero/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: linear-gradient(180deg, #A0ACE0 0%, #718096 100%) center center/cover;
+`;
+
+const Hero = () => {
+  return (
+    <Container>
+    </Container>
+  )
+}
+
+export default Hero;
