ecd6f61b141f55973cec1b976e01951391365e62
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 17:37:32 +0100

    This commit update button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,14 +4,16 @@ export const Button = styled.button`
   background: ${({primary}) => (primary ? "#667EEA" : "#AAFAFF")};
   padding: ${({big}) => (big ? " 	0.875rem 1.125rem" : " 	0.625rem 1rem")};
   font-size: ${({fontBig}) => (fontBig ? "1.125rem" : "1rem")};
-  border-radius: 0.8rem;
+  border-radius: 4px;
   color: #fff;
   white-space: nowrap;
   outline: 0;
   border: 0;
+  cursor: pointer;
 
   &:hover {
     transition: all 0.3s ease-out;
     background: ${({primary}) => (primary ? "#AAFAFF" : "#667EEA")};
+    color: #222;
   }
 `;
