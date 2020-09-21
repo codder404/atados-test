187e7bceb178226ea8a557a78ab88059d2df12dd
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 14:27:38 +0100

    This commit add navbar code

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,34 @@
 import React, { useState } from 'react';
 
+import logo from '../../assets/icons/Logo.svg';
+
 import * as S from './styles';
 
 function Navbar() {
   return (
     <S.Navbar>
       <S.NavbarContainer>
-        <S.NavbarLogo to="/" onClick={handleClick}>
-          
+        <S.NavbarLogo to="/">
+          <S.LogoImg src={logo} alt="Atatos" />
         </S.NavbarLogo>
         <S.MobileIcon>
 
         </S.MobileIcon>
         <S.NavbarMenu>
           <S.NavbarItems>
-            <S.NavbarLink to="/expole">Explore</S.NavbarLink>
+            <S.NavbarLink href="/">Explore</S.NavbarLink>
           </S.NavbarItems>
            <S.NavbarItems>
-            <S.NavbarLink to="/expole">Sobre nós</S.NavbarLink>
+            <S.NavbarLink href="/">Sobre nós</S.NavbarLink>
           </S.NavbarItems>
            <S.NavbarItems>
-            <S.NavbarLink to="/expole">Seguros</S.NavbarLink>
+            <S.NavbarLink href="/">Seguros</S.NavbarLink>
           </S.NavbarItems>
            <S.NavbarItems>
-            <S.NavbarLink to="/expole">Hóteis</S.NavbarLink>
+            <S.NavbarLink href="/">Hóteis</S.NavbarLink>
           </S.NavbarItems>
            <S.NavbarItems>
-            <S.NavbarLink to="/expole">Viagens</S.NavbarLink>
+            <S.NavbarLink href="/">Viagens</S.NavbarLink>
           </S.NavbarItems>
         </S.NavbarMenu>
       </S.NavbarContainer>
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -3,3 +3,17 @@ import styled from 'styled-components';
 export const Navbar = styled.nav`
   
 `;
+
+export const NavbarContainer = styled.div``;
+
+export const NavbarLogo = styled.div``;
+
+export const LogoImg = styled.img``;
+
+export const MobileIcon = styled.div``;
+
+export const NavbarMenu = styled.ul``;
+
+export const NavbarItems = styled.li``;
+
+export const NavbarLink = styled.a``;
